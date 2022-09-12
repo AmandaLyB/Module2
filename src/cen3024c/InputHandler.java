f018a56032101f63c25c2d09b69c9f3e9bf5b147
Amanda <abrown366@mail.valenciacollege.edu>
2022-09-12 15:03:48 -0400

    completed sort method

--- src/cen3024c/InputHandler.java
+++ src/cen3024c/InputHandler.java
@@ -11,80 +11,138 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.stream.*;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.lang.*;
 
 
 
 public class InputHandler {
 
+	// captures input from URL and stores modified text in file
 	public static void inputHandler () {
 		try {
-	        
 	        URL url = new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm");
-	        
 	        File file = new File("D:\\Valencia\\CEN 3024C SoftDev 1\\text.txt");
 	        PrintWriter out = new PrintWriter(new FileOutputStream(file, false));
 	        
 	        // read text returned by server
 	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-	        
 	        String line, result;
 	        while ((line = in.readLine()) != null) {
+	        	
+	        	// capture title text
 	        	if(line.contains("<h1>The Raven</h1>")) {
 	        		result = line.replaceAll("<[^>]*>", "");
-        			System.out.println(result); // prints title
         			out.println(result);
-	        		while ((line = in.readLine()) != null) {
+	        		
+        			// capture body of text
+        			while ((line = in.readLine()) != null) {
 	        			if(line.contains("<!--end chapter-->")) {
 	        				break;
 	        			}
-	        			result = line.replaceAll("<[^>]*>", "");
-	        			result = result.replaceAll("’", "'");
+	        			result = line.replaceAll("<[^>]*>", " "); // remove html coding
 	        			result = result.replaceAll("&mdash", " ");
-	        			result = result.replaceAll("[^A-Za-z0-9 ']", "");
-	        			System.out.println(result);
+	        			result = result.replaceAll("’", "");
+	        			result = result.replaceAll("[^A-Za-z0-9 ]", " "); // remove all non-word characters excluding space
+	        			result = result.replaceAll("\\s{2,}", " "); 
+
+	        			// print modified text from URL into file
 	        			out.println(result);
-	        			
 	        		}
 	        	}
 	        }
 	        in.close();
 	        out.close();
-	        
 	    }
 	    catch (MalformedURLException e) {
 	        System.out.println("Malformed URL: " + e.getMessage());
 	    }
 	    catch (IOException e) {
 	        System.out.println("I/O Error: " + e.getMessage());
-	    }
-	     
+	    }   
 	}
 
+	// reads and sorts text from file and returns frequency of words
 	public static void wordCount() {
 		
 		File file = new File("D:\\Valencia\\CEN 3024C SoftDev 1\\text.txt");
 		
 		try {
 			
-			File count = new File("D:\\Valencia\\CEN 3024C SoftDev 1\\text.txt");
+			File count = new File("D:\\Valencia\\CEN 3024C SoftDev 1\\count.txt");
 			BufferedReader in = new BufferedReader(new FileReader(file));
 			PrintWriter out = new PrintWriter(new FileOutputStream(count, false));
-			
 			String line;
-			while ((line = in.readLine()) != null) {
-				line.toLowerCase();
-				
+			HashMap<String,Integer> wordCount = new HashMap<String,Integer>();
+			String[] lineSplit;
 			
+			// converts text to lower case, splits words by space
+			while ((line = in.readLine()) != null) {
+				line = line.toLowerCase();
+				lineSplit = line.split(" ");
+
+				// creates new key and/or adds to count each instance of a word
+				for(int i = 0; i < lineSplit.length; ++i) {
+					if(wordCount.containsKey(lineSplit[i])) {
+						wordCount.put(lineSplit[i],wordCount.get(lineSplit[i])+1);
+					}
+					else {
+						wordCount.put(lineSplit[i],1);
+					}
+				}
 			}
 			
+			// remove empty key
+			Iterator<Entry<String, Integer>> it = wordCount.entrySet().iterator();
+			while (it.hasNext()) {
+			    Entry<String, Integer> e = it.next();
+			    //String key = e.getKey();
+			    Integer value = e.getValue();
+			    if (value == 51) {
+			        it.remove();
+			    }
+			}
+			
+			// sort by most frequent words
+			wordCount = sortByValue(wordCount);
+			for(String i : wordCount.keySet()) {
+				out.println(i + " " + wordCount.get(i));
+			}
+			in.close();
+			out.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-					
+			e.printStackTrace();		
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+		}	
 	}
+	
+	// sorts HashMap of word-frequency pairs in order of most to least frequent and returns new sorted HashMap
+	public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer> > list =
+               new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
+ 
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+        Collections.reverse(list);
+        
+        // put data from sorted list to new HashMap
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> word : list) {
+            temp.put(word.getKey(), word.getValue());
+        }
+        return temp;
+    }
 }
--- src/cen3024c/TextAnalyzer.java
+++ src/cen3024c/TextAnalyzer.java
@@ -6,6 +6,7 @@ public class TextAnalyzer {
 		// TODO Auto-generated method stub
 
 		InputHandler.inputHandler();
+		InputHandler.wordCount();
 	}
 
 }
